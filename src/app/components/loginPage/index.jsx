6d73f5769e421c9d4378e97b96136af98c43775d
Diego sales <diegofelipesales23@gmail.com>
2023-12-26 15:00:11 -0400

    finalizando ui de login

--- src/app/components/loginPage/index.jsx
+++ src/app/components/loginPage/index.jsx
@@ -1,24 +1,31 @@
-'use client';
+"use client";
+import styles from "./loginPage.module.scss";
 
-import styles from './loginPage.module.scss';
 import { FaBook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { PiFilePdfBold } from "react-icons/pi";
 
 function LoginPage() {
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
-        <FaBook size={150} color='#ff5454'/>
+        <div className={styles.logo}>
+          <FaBook className={styles.svgLogo} />
+          <PiFilePdfBold className={styles.svgLogo2} />
+        </div>
         <h1>BookGram</h1>
       </div>
       <div className={styles.btnContainer}>
         <button>
           Entrar com o Google
-          <FcGoogle size={20} />
+          <FcGoogle size={25} />
         </button>
       </div>
       <div className={styles.textContainer}>
-        <p>Armazene seus livros em formato PDF e desfrute da leitura em qualquer lugar!</p>
+        <p>
+          Armazene seus livros em formato PDF e desfrute da leitura em qualquer
+          lugar!
+        </p>
       </div>
     </div>
   );
