4c9e0da2502152bd8dbe414e2f9f4e2ff0144f59
Diego sales <diegofelipesales23@gmail.com>
2023-12-29 22:35:28 -0400

    modificando cores e posição dos botoes add, editar e ler

--- src/app/components/listComponent/index.jsx
+++ src/app/components/listComponent/index.jsx
@@ -2,10 +2,11 @@
 import Image from "next/image";
 import styles from "./list.module.scss";
 
-import { FaBookReader } from "react-icons/fa";
-import { MdEdit,MdDeleteSweep  } from "react-icons/md";
-import hpFenix from '../../assets/hpfenix.jpeg';
-import semcapa from '../../assets/semcapa1.png';
+import {FaEdit, FaTrash} from "react-icons/fa";
+import { BiSolidSpreadsheet } from "react-icons/bi";
+
+import hpFenix from "../../assets/hpfenix.jpeg";
+import semcapa from "../../assets/default.png";
 
 function ListComponent() {
   return (
@@ -14,111 +15,153 @@ function ListComponent() {
         <h1>Meus livros</h1>
         <ul className={styles.list}>
           <li>
-            {
-              !hpFenix && <Image src={hpFenix} width={150} height={150} />
-            }
-            {
-              hpFenix && <Image src={semcapa} width={150} height={150} />
-            }
-            
+            <span className={styles.deleteButton}>
+                <FaTrash  size={20} color="#CD5C5C"  />   
+            </span>
+            {!hpFenix && <Image src={hpFenix} width={150} height={150}  />}
+            {hpFenix && <Image src={semcapa} width={125} height={125} alt="img do livro default" />}
             <div>
               <h2>Harry potter e a ordem da fenix</h2>
               <p>Autor: J.K Rowling</p>
             </div>
             <div className={styles.buttons}>
-              <FaBookReader size={22} />
-              <MdEdit size={25} />
-              <MdDeleteSweep size={25} />
+              <span>
+                <BiSolidSpreadsheet size={25} />
+                <p>ler</p>
+              </span>
+              <span>
+                <FaEdit size={25} />
+                <p>editar</p>
+              </span>
+              {/* <span>
+                <RiDeleteBin2Fill size={25} />
+                <p>excluir</p>
+              </span> */}
             </div>
           </li>
           <li>
-            {
-              !hpFenix && <Image src={hpFenix} width={150} height={150} />
-            }
-            {
-              hpFenix && <Image src={semcapa} width={150} height={150} />
-            }
-            
+            <span className={styles.deleteButton}>
+                <FaTrash  size={20} color="#CD5C5C"  />   
+            </span>
+            {!hpFenix && <Image src={hpFenix} width={150} height={150}  />}
+            {hpFenix && <Image src={semcapa} width={125} height={125} alt="img do livro default" />}
             <div>
               <h2>Harry potter e a ordem da fenix</h2>
               <p>Autor: J.K Rowling</p>
             </div>
             <div className={styles.buttons}>
-              <FaBookReader size={22} />
-              <MdEdit size={25} />
-              <MdDeleteSweep size={25} />
+              <span>
+                <BiSolidSpreadsheet size={25} />
+                <p>ler</p>
+              </span>
+              <span>
+                <FaEdit size={25} />
+                <p>editar</p>
+              </span>
+              {/* <span>
+                <RiDeleteBin2Fill size={25} />
+                <p>excluir</p>
+              </span> */}
             </div>
           </li>
           <li>
-            {
-              !hpFenix && <Image src={hpFenix} width={150} height={150} />
-            }
-            {
-              hpFenix && <Image src={semcapa} width={150} height={150} />
-            }
-            
+            <span className={styles.deleteButton}>
+                <FaTrash  size={20} color="#CD5C5C"  />   
+            </span>
+            {!hpFenix && <Image src={hpFenix} width={150} height={150}  />}
+            {hpFenix && <Image src={semcapa} width={125} height={125} alt="img do livro default" />}
             <div>
               <h2>Harry potter e a ordem da fenix</h2>
               <p>Autor: J.K Rowling</p>
             </div>
             <div className={styles.buttons}>
-              <FaBookReader size={22} />
-              <MdEdit size={25} />
-              <MdDeleteSweep size={25} />
+              <span>
+                <BiSolidSpreadsheet size={25} />
+                <p>ler</p>
+              </span>
+              <span>
+                <FaEdit size={25} />
+                <p>editar</p>
+              </span>
+              {/* <span>
+                <RiDeleteBin2Fill size={25} />
+                <p>excluir</p>
+              </span> */}
             </div>
           </li>
           <li>
-            {
-              !hpFenix && <Image src={hpFenix} width={150} height={150} />
-            }
-            {
-              hpFenix && <Image src={semcapa} width={150} height={150} />
-            }
-            
+            <span className={styles.deleteButton}>
+                <FaTrash  size={20} color="#CD5C5C"  />   
+            </span>
+            {!hpFenix && <Image src={hpFenix} width={150} height={150}  />}
+            {hpFenix && <Image src={semcapa} width={125} height={125} alt="img do livro default" />}
             <div>
               <h2>Harry potter e a ordem da fenix</h2>
               <p>Autor: J.K Rowling</p>
             </div>
             <div className={styles.buttons}>
-              <FaBookReader size={22} />
-              <MdEdit size={25} />
-              <MdDeleteSweep size={25} />
+              <span>
+                <BiSolidSpreadsheet size={25} />
+                <p>ler</p>
+              </span>
+              <span>
+                <FaEdit size={25} />
+                <p>editar</p>
+              </span>
+              {/* <span>
+                <RiDeleteBin2Fill size={25} />
+                <p>excluir</p>
+              </span> */}
             </div>
           </li>
           <li>
-            {
-              !hpFenix && <Image src={hpFenix} width={150} height={150} />
-            }
-            {
-              hpFenix && <Image src={semcapa} width={150} height={150} />
-            }
-            
+            <span className={styles.deleteButton}>
+                <FaTrash  size={20} color="#CD5C5C"  />   
+            </span>
+            {!hpFenix && <Image src={hpFenix} width={150} height={150}  />}
+            {hpFenix && <Image src={semcapa} width={125} height={125} alt="img do livro default" />}
             <div>
               <h2>Harry potter e a ordem da fenix</h2>
               <p>Autor: J.K Rowling</p>
             </div>
             <div className={styles.buttons}>
-              <FaBookReader size={22} />
-              <MdEdit size={25} />
-              <MdDeleteSweep size={25} />
+              <span>
+                <BiSolidSpreadsheet size={25} />
+                <p>ler</p>
+              </span>
+              <span>
+                <FaEdit size={25} />
+                <p>editar</p>
+              </span>
+              {/* <span>
+                <RiDeleteBin2Fill size={25} />
+                <p>excluir</p>
+              </span> */}
             </div>
           </li>
           <li>
-            {
-              !hpFenix && <Image src={hpFenix} width={150} height={150} />
-            }
-            {
-              hpFenix && <Image src={semcapa} width={150} height={150} />
-            }
-            
+            <span className={styles.deleteButton}>
+                <FaTrash  size={20} color="#CD5C5C"  />   
+            </span>
+            {!hpFenix && <Image src={hpFenix} width={150} height={150}  />}
+            {hpFenix && <Image src={semcapa} width={125} height={125} alt="img do livro default" />}
             <div>
               <h2>Harry potter e a ordem da fenix</h2>
               <p>Autor: J.K Rowling</p>
             </div>
             <div className={styles.buttons}>
-              <FaBookReader size={22} />
-              <MdEdit size={25} />
-              <MdDeleteSweep size={25} />
+              <span>
+                <BiSolidSpreadsheet size={25} />
+                <p>ler</p>
+              </span>
+              <span>
+                <FaEdit size={25} />
+                <p>editar</p>
+              </span>
+              {/* <span>
+                <RiDeleteBin2Fill size={25} />
+                <p>excluir</p>
+              </span> */}
             </div>
           </li>
         </ul>
