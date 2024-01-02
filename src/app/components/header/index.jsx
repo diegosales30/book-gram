296ae237ea73af1f2b0ac2657a418fabaed198e8
Diego sales <diegofelipesales23@gmail.com>
2024-01-01 22:35:55 -0400

    finalizando modal de logout com nome e foto de perfil

--- src/app/components/header/index.jsx
+++ src/app/components/header/index.jsx
@@ -5,7 +5,6 @@ import styles from "./header.module.scss";
 
 //context
 import { useLoggedContext } from "@/app/context/loginContext";
-//import { useDarkContext } from "@/app/context/darkContext";
 
 import Image from "next/image";
 
@@ -14,44 +13,45 @@ import profileImg from "../../assets/image.jpeg";
 import { MdOutlineLogout, MdOutlineManageSearch } from "react-icons/md";
 import SearchComponent from "../searchComponent";
 import AddBookComponent from "../addBookComponent";
-//import { MdNightsStay, MdWbSunny } from "react-icons/md";
-
+import { IoMdCloseCircle } from "react-icons/io";
+import { LuFileSearch } from "react-icons/lu";
+import { FaRegQuestionCircle } from "react-icons/fa";
 
 function Header() {
   const { setLogged } = useLoggedContext();
   const [searchMobile, setSearchMobile] = useState(false);
+  const [logoutModal, setLogoutModal] = useState(false);
   const searchMobileRef = useRef(null);
-  //const {dark, setDark } = useDarkContext();
-  
+
   const handleSearchMobile = () => {
     setSearchMobile(true);
-    
   };
 
   const handleClickOutside = (event) => {
-    if (searchMobileRef.current && !searchMobileRef.current.contains(event.target)) {
+    if (
+      searchMobileRef.current &&
+      !searchMobileRef.current.contains(event.target)
+    ) {
       // Clicou fora do modal, fecha o modal
       setSearchMobile(false);
-      
     }
   };
-
   useEffect(() => {
     // Adiciona o event listener quando o componente monta
-    document.addEventListener('click', handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
     // Remove o event listener quando o componente desmonta
     return () => {
-      document.removeEventListener('click', handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, [searchMobile]);
+
+  const handleLogoutModal = () => {
+    setLogoutModal(!logoutModal);
+  };
 
   const handleLogout = () => {
     setLogged(false);
   };
-  //modo noturno desativado
-  // const handleSwitchMode = () => {
-  //   setDark(!dark);
-  // };
 
   return (
     <header className={styles.headerContainer}>
@@ -63,37 +63,60 @@ function Header() {
         <MdOutlineManageSearch size={30} color="#5f6368" />
         <p>Pesquisar</p>
       </button>
-      {
-        searchMobile && (
-          <div className={styles.searchMobileContainer} ref={searchMobileRef}>
-            <SearchComponent />
-          </div>
-        )
-      }
+      {searchMobile && (
+        <div className={styles.searchMobileContainer} ref={searchMobileRef}>
+          <SearchComponent />
+        </div>
+      )}
       <div className={styles.headerContent}>
         <div className={styles.headerProfile}>
-        <Image src={profileImg} width={35} height={35} alt="profile image" />
-        {/* <p>John Doe</p> */}
-        </div>
-        <div className={styles.headerBtn}>
-        {/* <div className={styles.nightMode}>
-          {dark && (
-            <>
-              <MdWbSunny size={20} onClick={handleSwitchMode} />
-              <p>Light Mode</p>
-            </>
-          )}
-          {!dark && (
-            <>
-              <MdNightsStay size={20} onClick={handleSwitchMode} />
-              <p>Dark Mode</p>
-            </>
-          )}
-        </div> */}
-          <button onClick={handleLogout}>
-            <MdOutlineLogout size={30} color="#5f6368" />
+          <button onClick={handleLogoutModal}>
+            <Image
+              src={profileImg}
+              width={35}
+              height={35}
+              alt="profile image"
+            />
           </button>
         </div>
+        {logoutModal && (
+          <div className={styles.logoutContainer}>
+            <div className={styles.logoutModal}>
+              <IoMdCloseCircle
+                size={25}
+                onClick={handleLogoutModal}
+                color="#CD5C5C"
+                className={styles.closeIcon}
+              />
+              <header className={styles.logoutHeader}>
+                <Image
+                  src={profileImg}
+                  width={35}
+                  height={35}
+                  alt="profile image"
+                />
+                <p>Olá, John!</p>
+              </header>
+              <div className={styles.logoutBtns}>
+                <button>
+                  <LuFileSearch size={20}  />
+                  <p>Livros públicos</p>
+                </button>
+                <button>
+                  <FaRegQuestionCircle  size={20}  />
+                  <p>Sobre o Projeto</p>
+                  </button>
+                <button onClick={handleLogout}>
+                  <MdOutlineLogout size={23}  />
+                  <p>Sair do bookgram</p>
+                </button>
+              </div>
+              
+
+            </div>
+          </div>
+        )}
+        <div className={styles.headerBtn}></div>
       </div>
     </header>
   );
