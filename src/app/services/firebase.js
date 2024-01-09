cc350ae308bc968f590dd99ee7d4eac82836281f
Diego sales <diegofelipesales23@gmail.com>
2024-01-09 13:49:08 -0400

    omitindo conf do firebase

--- src/app/services/firebase.js
+++ src/app/services/firebase.js
@@ -13,19 +13,19 @@ const firebaseConfig = {
     // appId: process.env.REACT_FIREBASE_APP_ID,
     // measurementId: process.env.REACT_FIREBASE_MEASUREMENT_ID
     
-  apiKey: "AIzaSyC4BeVtImn5Be8kxwB_sqWzxYpFjFdO6Xg",
+  // apiKey: "AIzaSyC4BeVtImn5Be8kxwB_sqWzxYpFjFdO6Xg",
 
-  authDomain: "bookgram-b35c2.firebaseapp.com",
+  // authDomain: "bookgram-b35c2.firebaseapp.com",
 
-  projectId: "bookgram-b35c2",
+  // projectId: "bookgram-b35c2",
 
-  storageBucket: "bookgram-b35c2.appspot.com",
+  // storageBucket: "bookgram-b35c2.appspot.com",
 
-  messagingSenderId: "711092446596",
+  // messagingSenderId: "711092446596",
 
-  appId: "1:711092446596:web:4d19a0efa07e485730e9d1",
+  // appId: "1:711092446596:web:4d19a0efa07e485730e9d1",
 
-  measurementId: "G-CW5B7BDY5Z"
+  // measurementId: "G-CW5B7BDY5Z"
 };
 
 const app = initializeApp(firebaseConfig);
