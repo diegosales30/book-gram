a09f628fd825045c441e284ec1307bad79e865bc
Diego sales <diegofelipesales23@gmail.com>
2024-01-09 13:52:30 -0400

    dotenv

--- src/app/services/firebase.js
+++ src/app/services/firebase.js
@@ -12,20 +12,6 @@ const firebaseConfig = {
     // messagingSenderId: process.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
     // appId: process.env.REACT_FIREBASE_APP_ID,
     // measurementId: process.env.REACT_FIREBASE_MEASUREMENT_ID
-    
-  // apiKey: "AIzaSyC4BeVtImn5Be8kxwB_sqWzxYpFjFdO6Xg",
-
-  // authDomain: "bookgram-b35c2.firebaseapp.com",
-
-  // projectId: "bookgram-b35c2",
-
-  // storageBucket: "bookgram-b35c2.appspot.com",
-
-  // messagingSenderId: "711092446596",
-
-  // appId: "1:711092446596:web:4d19a0efa07e485730e9d1",
-
-  // measurementId: "G-CW5B7BDY5Z"
 };
 
 const app = initializeApp(firebaseConfig);
